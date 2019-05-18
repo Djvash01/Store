644f330f2681c4e051c8dab07f2787c5f498e096
David Yepes <djvash01@gmail.com>
2019-05-18 16:27:49 -0500

    Agregando control por categoria si ya existe

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -9,9 +9,18 @@ router.get('/', async(req, res)=>{
 
 router.post('/',async(req, res)=>{
     const { name } = req.body;
-    const newCategory = new Category({name});
-    await newCategory.save();
-    res.json(newCategory);
+    const category = await Category.findOne({name:name});
+    if(!category){
+        const newCategory = new Category({name});
+        await newCategory.save();
+        res.json(newCategory); 
+    }else{
+        res.json({
+            "code":"1001",
+            'description':'Categoria ya existente'
+        });
+    }
+   
 });
 
 router.delete('/:id',async(req,res)=>{
