7691681fa8ee7cfcb319d00d7ab5550d53643088
David Yepes <djvash01@gmail.com>
2019-06-15 12:11:55 -0500

    agregando cosas de carlos no mames

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -11,38 +11,35 @@ router.get('/', async(req, res)=>{
 router.post('/',async(req, res)=>{
     const { name } = req.body;
     const duplicate = await Category.findOne({name:name});
-    errors=[];
     if(!duplicate){
         const category = new Category({name});
         await category.save();
         res.json({
             category,
-            errors
+            'error':{}
         }); 
     }else{
         error={
             "code":"1001",
             'description':'Categoria ya existente'
         };
-        errors.push(error);
-        res.json({'category':[], errors});
+        res.json({'category':{}, error});
     }
    
 });
 
 router.delete('/:name',async(req,res)=>{
     const products = await Product.findOne({'category.name':req.params.name});
-    errors=[];
+   
     if(!products){
         const category = await Category.findOneAndDelete({name:req.params.name});
-        res.json({category,errors});
+        res.json({category,'error':{}});
     }else{
         error={
             'code':'1000',
             'description':'La categoria no puede ser eliminada por que tiene items asociados.'
         };
-        errors.push(error);
-        res.json({'category':[],errors });
+        res.json({'category':{},error });
     }
     
     
