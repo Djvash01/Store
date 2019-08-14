aee11e37af41ac10e11958ec7750753f2dd5e8e5
David Yepes <djvash01@gmail.com>
2019-08-13 21:08:41 -0500

    Eliminando code del modelo producto

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -2,11 +2,10 @@ const { Schema, model } = require('mongoose');
 const CategorySchema = require('../models/Category').schema;
 
 const ProductSchema = new Schema({
-    code: { type: String, required: true },
     name: { type: String, required: true },
     details: { type: String, required: true },
     price: { type: String, required: true },
-    category: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
+    categories: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
     imagePath: { type: String },
     created_at: { type: Date, default: Date.now }
 });
