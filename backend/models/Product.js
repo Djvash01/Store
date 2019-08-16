73f7915cb6b56e0a2842899f2a887636dd77301d
David Yepes <djvash01@gmail.com>
2019-08-15 19:20:42 -0500

    Corrigiendo schema de productos

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,9 +1,9 @@
-const { Schema, model, Decimal128 } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const ProductSchema = new Schema({
     name: { type: String, required: true },
     details: { type: String, required: true },
-    price: { type: Decimal128, required: true },
+    price: { type: String, required: true },
     categories: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
     imagePath: { type: String },
     created_at: { type: Date, default: Date.now }
